4f9f6b00fe3119738388761ffcaa5cf4e26c22b4
szmulinho <samik1997@gmail.com>
2023-10-11 14:12:14 +0200

    model update

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
 FROM golang:1.21.1-alpine AS build
 
-WORKDIR /app
+WORKDIR /users
 COPY . .
 
 RUN apk add --no-cache git
 RUN go mod download
-RUN CGO_ENABLED=0 GOOS=linux go build -o app
+RUN CGO_ENABLED=0 GOOS=linux go build -o users
 
 FROM alpine:latest
 
-WORKDIR /app
-COPY --from=build /app/app /app/app
+WORKDIR /users
+COPY --from=build /users/users /users/users
 
 EXPOSE 8092
 
-CMD ["./app"]
+CMD ["./users"]
--- internal/model/model.go
+++ internal/model/model.go
@@ -6,28 +6,14 @@ import (
 
 var JwtKey = []byte(os.Getenv("JWT_KEY"))
 
-type JwtToken struct {
-	Token string `json:"token"`
-}
-
 type Exception struct {
 	Message string `json:"message"`
-}
-
-type Response struct {
-	Data string `json:"data"`
 }
 
 type User struct {
 	ID       int64  `gorm:"primaryKey;autoIncrement"`
 	Login    string `gorm:"unique" json:"login"`
 	Password string `json:"password"`
-	Role     string `json:"role"`
-}
-
-type JwtUser struct {
-	Jwt      string "jwt"
-	Password string "password"
 	Role     string `json:"role"`
 }
 
