e47e585980e3b9ef30a3ccb8155317233753225a
szmulinho <samik1997@gmail.com>
2023-10-12 15:01:48 +0200

    v

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
-	github.com/szmulinho/common v0.0.0-20231012123232-9f22015ecb40
+	github.com/szmulinho/common v0.0.0-20231012123959-a62ad2362c7c
 	golang.org/x/crypto v0.13.0
 	gorm.io/gorm v1.25.5
 )
--- vendor/github.com/szmulinho/common/config/config.go
+++ vendor/github.com/szmulinho/common/config/config.go
@@ -10,6 +10,12 @@ func init() {
 	if err := godotenv.Load(); err != nil {
 		fmt.Println("Error loading .env file")
 	}
+
+	fmt.Println("DB_HOST:", os.Getenv("DB_HOST"))
+	fmt.Println("DB_USER:", os.Getenv("DB_USER"))
+	fmt.Println("DB_PASSWORD:", os.Getenv("DB_PASSWORD"))
+	fmt.Println("DB_NAME:", os.Getenv("DB_NAME"))
+	fmt.Println("DB_PORT:", os.Getenv("DB_PORT"))
 }
 
 type StorageConfig struct {
